3d3a1f91a946b3ee04ee2ecadfae55b0ef190ba5
bryanregie <bryanregie@hotmail.com>
2018-01-03 16:41:14 +0700

    sourcetree

--- testrep/Form1.vb
+++ testrep/Form1.vb
@@ -2,5 +2,7 @@
 
     Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
         MsgBox("test")
+        'sdsds
+        MsgBox("kedua")
     End Sub
 End Class
